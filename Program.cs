e4f9ac457f9c28378e9e8c1d53ed15171ce3bb28
cathysadykh <81162530+cathysadykh@users.noreply.github.com>
2024-12-05 13:25:13 +0000

    get pizza by id

--- Program.cs
+++ Program.cs
@@ -35,4 +35,5 @@ app.MapPost("/pizza", async (PizzaDb db, Pizza pizza) =>
     await db.SaveChangesAsync();
     return Results.Created($"/pizza/{pizza.IdEhod}", pizza);
 });
+app.MapGet("/pizza/{id}", async (PizzaDb db, int id) => await db.Pizzas.FindAsync(id));
 app.Run();
